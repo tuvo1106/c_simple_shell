01d228483afb01d60f52178026599f251ab5e161
JosefGoodyear <josefgoodyear@gmail.com>
2019-04-05 22:59:01 +0000

    Add headers

---
+++ holberton.h
@@ -0,0 +1,21 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+_Bool isSpace(char c);
+unsigned int countWords(char *s);
+char **split(char *str);
+void print_tab(char **tab);
+int _strlen(char *s);
+char *_strdup(char *str);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+
+#endif
