ce9f9cb38caafc5597d79e87c0fc4f1ce9e15c60
JosefGoodyear <josefgoodyear@gmail.com>
2019-04-05 22:55:25 +0000

    Add simple shell base

---
+++ shell.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+
+int main(void)
+{
+	pid_t f1;
+	size_t n = 0;
+	char *buffer = NULL;
+	char **args;
+	int len, check, j, i = 0;
+
+	while (1)
+	{
+		i++;
+		write(STDOUT_FILENO, "$ ", 2);
+		len = getline(&buffer, &n, stdin);
+		if (len < 0)
+		{
+			free(buffer);
+			write(STDOUT_FILENO, "\n", 1);
+			return (0);
+		}
+		buffer[len - 1] = 0;
+		args = split(buffer);
+		f1 = fork();
+		if (f1 == 0)
+		{
+			check = execve(args[0], args, NULL);
+			if (check == -1)
+			{
+				write(STDOUT_FILENO, "command not found\n", 18);
+				free(buffer);
+				exit(0);
+			}
+
+		}
+		else
+		{
+			wait(NULL);
+			j = 0;
+			while (args[j])
+			{
+				free(args[j]);
+				j++;
+			}
+			free(args);
+		}
+	}
+	free(buffer);
+}
+
---
+++ split_string.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+
+_Bool isSpace(char c)
+{
+	return (c == ' ');
+}
+
+unsigned int countWords(char *s)
+{
+	int wordOn = false;
+	int words = 0;
+
+	while (*s)
+	{
+		if (isSpace(*s) && wordOn)
+			wordOn = false;
+		else if (!isSpace(*s) && !wordOn)
+		{
+			wordOn = true;
+			words++;
+		}
+		s++;
+	}
+	return (words);
+}
+
+char **splitString(char *str)
+{
+	char *cpy = _strdup(str);
+	char **res;
+	char *tok;
+	unsigned int i = 0;
+
+	res = malloc((countWords(str) + 1) * 8);
+	tok = strtok(cpy, " ");
+
+	while (tok != NULL)
+	{
+		res[i] = _strdup(tok);
+		tok = strtok(NULL, " ");
+		i++;
+	}
+	res[i] = NULL;
+	free(cpy);
+	return (res);
+}
