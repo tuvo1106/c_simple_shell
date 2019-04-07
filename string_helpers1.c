87d8b6b381fd6f6fdc230157ca59a67954449760
Tu Vo <tuvo1106@gmail.com>
2019-04-07 17:55:15 +0000

    Refactored string helpers

---
+++ string_helpers1.c
@@ -0,0 +1,76 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string s
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	char *p = s;
+
+	while (*s)
+		s++;
+	return (s - p);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: copy destination
+ * @src: copy source
+ * Return: concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest + _strlen(dest);
+
+	while (*src)
+		*ptr++ = *src++;
+	*ptr = 0;
+	return (dest);
+}
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 if true, 0 if false
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1)
+	{
+		if (*s1 != *s2)
+			break;
+		s1++;
+		s2++;
+	}
+
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: string to be copied
+ * Return: copied string
+ */
+char *_strdup(char *str)
+{
+	int i, len;
+	char *copy;
+
+	if (!str)
+		return (NULL);
+	len = _strlen(str);
+	copy = malloc(sizeof(char) * len + 1);
+	if (!copy)
+	{
+		perror("Malloc failed\n");
+		exit(errno);
+	}
+	for (i = 0; i < len; i++)
+		copy[i] = str[i];
+	copy[i] = 0;
+	return (copy);
+}
---
+++ string_helpers2.c
@@ -0,0 +1,66 @@
+#include "holberton.h"
+
+/**
+ * _strtok - tokenizes strings at delimiter
+ * @str: input string
+ * @delim: delimiter
+ * Return: pointer to start of string
+ */
+char *_strtok(char *str, char *delim)
+{
+	static char *lastptr;
+	char ch;
+
+	if (str == NULL)
+		str = lastptr;
+	do {
+		ch = *str++;
+		if (!ch)
+			return (NULL);
+	} while (_strchr(delim, ch));
+	str--;
+	lastptr = str + _strcspn(str, delim);
+	if (*lastptr)
+		*lastptr++ = 0;
+	return (str);
+}
+
+/**
+ * _strcspn - returns first occurence of any char in
+ * second string in first string
+ * @string: input string to search
+ * @chars: input chars to check
+ * Return: pointer to first match
+ */
+int _strcspn(char *string, char *chars)
+{
+	char c;
+	char *p, *s;
+
+	for (s = string, c = *s; c; s++, c = *s)
+		for (p = chars; *p; p++)
+			if (c == *p)
+				return (s - string);
+	return (s - string);
+}
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to be searched
+ * @c: target char
+ * Return: pointer to first occurrence of c or NULL if char not found
+ */
+char *_strchr(char *s, char c)
+{
+	char x;
+
+	while (true)
+	{
+		x = *s++;
+		if (x == c)
+			return (s - 1);
+		if (!x)
+			return (NULL);
+	}
+}
+
