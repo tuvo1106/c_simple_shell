f42b2b93351394ad6541a861f027601fc5088788
JosefGoodyear <josefgoodyear@gmail.com>
2019-04-08 16:57:46 +0000

    Betty

--- built_ins.c
+++ built_ins.c
@@ -1,6 +1,11 @@
 #include "holberton.h"
 
-
+/**
+* builtIns - find the right built in to use
+* @args: parsed strings
+*
+* Return: 1 if found, 0 if not found
+*/
 int builtIns(char **args)
 {
 	type_b getBuiltIns[] = {
@@ -14,7 +19,7 @@ int builtIns(char **args)
 	while (getBuiltIns[index].command)
 	{
 		if (_strcmp(args[0], getBuiltIns[index].command) == 0)
-		{	
+		{
 			getBuiltIns[index].func(args);
 			return (1);
 		}
@@ -23,12 +28,23 @@ int builtIns(char **args)
 	return (0);
 }
 
+/**
+* exitFunc - exit.
+* @args: the parsed exit string
+*
+* Return: 1
+*/
 int exitFunc(char **args)
 {
-	printf("exit: success\n");	
+	printf("exit: success\n");
 	return (1);
 }
-
+/**
+* envFunc - print the environment
+* @args: the parsed env string
+*
+* Return: 1
+*/
 int envFunc(char **args)
 {
 	printf("env: success\n");
